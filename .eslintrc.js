eccf3ceb46774b2a67f07446537d96f066f0e09b
Supparad Wannathippakorn <supparad.w@psru.ac.th>
2021-11-16 00:10:47 +0700

    zimpo4

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,19 @@
 module.exports = {
-  root: true,
-  env: {
-    browser: true,
-    node: true
-  },
-  parserOptions: {
-    parser: 'babel-eslint'
-  },
-  extends: [
-    '@nuxtjs',
-    'plugin:nuxt/recommended'
-  ],
-  // add your custom rules here
-  rules: {
-    'nuxt/no-cjs-in-config': 'off'
-  }
+    root: true,
+    env: {
+        browser: true,
+        node: true
+    },
+    parserOptions: {
+        parser: 'babel-eslint'
+    },
+    extends: [
+        '@nuxtjs',
+        "prettier",
+        'plugin:nuxt/recommended'
+    ],
+    // add your custom rules here
+    rules: {
+        'nuxt/no-cjs-in-config': 'off'
+    }
 }
