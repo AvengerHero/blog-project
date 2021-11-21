f79aafe7e0111995b864443bae2e4b13e8ee20ea
parinya <dagonparinya@gmail.com>
2021-11-21 20:44:11 +0700

    .

--- pages/home/index.vue
+++ pages/home/index.vue
@@ -42,15 +42,8 @@ export default {
   },
   data: () => {
     return {
-      articleData: [],
-      colors: [
-        'indigo',
-        'warning',
-        'pink darken-2',
-        'red lighten-1',
-        'deep-purple accent-4'
-      ],
-      slides: ['First', 'Second', 'Third', 'Fourth', 'Fifth']
+      articleData: []
+
     }
   },
   created () {
